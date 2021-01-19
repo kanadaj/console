4e9c0ff161f23bc52c38577144a51e1d3103bdd1
Daniel Rusznyak <daniel@rusznyak.com>
2021-01-19 18:25:50 +0000

    Remove the Multis CNI requirement

--- frontend/public/components/pod.tsx
+++ frontend/public/components/pod.tsx
@@ -544,7 +544,8 @@ const PodGraphs = requirePrometheus(({ pod }) => {
             title={t('workload~Network in')}
             humanize={humanizeDecimalBytesPerSec}
             namespace={pod.metadata.namespace}
-            query={`(sum(irate(container_network_receive_bytes_total{pod='${pod.metadata.name}', namespace='${pod.metadata.namespace}'}[5m])) by (pod, namespace, interface)) + on(namespace,pod,interface) group_left(network_name) ( pod_network_name_info )`}
+            query={`(sum(irate(container_network_receive_bytes_total{pod='${pod.metadata.name}', namespace='${pod.metadata.namespace}'}[5m])) by (pod, namespace, interface))`}
+            //query={`(sum(irate(container_network_receive_bytes_total{pod='${pod.metadata.name}', namespace='${pod.metadata.namespace}'}[5m])) by (pod, namespace, interface)) + on(namespace,pod,interface) group_left(network_name) ( pod_network_name_info )`}
             description={getNetworkName}
           />
         </div>
@@ -553,7 +554,8 @@ const PodGraphs = requirePrometheus(({ pod }) => {
             title={t('workload~Network out')}
             humanize={humanizeDecimalBytesPerSec}
             namespace={pod.metadata.namespace}
-            query={`(sum(irate(container_network_transmit_bytes_total{pod='${pod.metadata.name}', namespace='${pod.metadata.namespace}'}[5m])) by (pod, namespace, interface)) + on(namespace,pod,interface) group_left(network_name) ( pod_network_name_info )`}
+            query={`(sum(irate(container_network_transmit_bytes_total{pod='${pod.metadata.name}', namespace='${pod.metadata.namespace}'}[5m])) by (pod, namespace, interface))`}
+            //query={`(sum(irate(container_network_transmit_bytes_total{pod='${pod.metadata.name}', namespace='${pod.metadata.namespace}'}[5m])) by (pod, namespace, interface)) + on(namespace,pod,interface) group_left(network_name) ( pod_network_name_info )`}
             description={getNetworkName}
           />
         </div>
